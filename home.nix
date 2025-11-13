924f400a37f300ac5de235d0dbf0a7afedf3380e
bottlenix <bottlenix@gmail.com>
2025-11-13 12:43:51 -0500

    install vscode

--- home.nix
+++ home.nix
@@ -35,6 +35,24 @@
     };
   };
 
+  programs.vscode = {
+      enable = true;
+  
+      profiles.default.userSettings = {
+          # This property will be used to generate settings.json:
+          # https://code.visualstudio.com/docs/getstarted/settings#_settingsjson
+          # "editor.formatOnSave" = true;
+      };
+      # keybindings = [
+          # See https://code.visualstudio.com/docs/getstarted/keybindings#_advanced-customization
+          # {
+              # key = "shift+cmd+j";
+              # command = "workbench.action.focusActiveEditorGroup";
+              # when = "terminalFocus";
+          # }
+      # ];
+  };
+
 #  services.gpg-agent = {
 #    enable = true;
 #    defaultCacheTtl = 1800;
