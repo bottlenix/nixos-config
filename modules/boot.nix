39894c2421b60ef55323228a2659e1e82b7ea2de
bottlenix <bottlenix@gmail.com>
2025-11-15 09:25:05 -0500

    turn off osprober

--- modules/boot.nix
+++ modules/boot.nix
@@ -4,7 +4,7 @@
   # Bootloader configuration
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.useOSProber = false;
 
   # Use latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
